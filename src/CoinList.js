ce093c9a45bc652a70f054b326f47997de804b06
XINYUAN YAN <loki.yxy@hotmail.com>
2018-10-01 18:10:27 +1000

    update coinlist on settings page

--- src/CoinList.js
+++ src/CoinList.js
@@ -18,18 +18,25 @@ const CoinTitle = styled.div`
   }
 `
 
+const CoinHeaderGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`
+
+const CoinSymbol = styled.div`
+  justify-self: right;
+`
+
 export default function() {
   return <CoinGrid>
-    {Object.keys(this.state.coinList).map(coin => 
+    {Object.keys(this.state.coinList).slice(0,50).map(coin => 
       <CoinTitle>
-        {coin}
+        <CoinHeaderGrid>
+          <div>{this.state.coinList[coin].CoinName}</div>
+          <CoinSymbol>{this.state.coinList[coin].Symbol}</CoinSymbol>
+        </CoinHeaderGrid>
+        <div>{<img style={{height: '50px'}} src={`http://cryptocompare.com/${this.state.coinList[coin].ImageUrl}`} />}</div>
       </CoinTitle>
     )}
   </CoinGrid>
 }
-
-// export default function() {
-//   return <div>
-//     {Object.keys(this.state.coinList).length}
-//   </div>
-// }
